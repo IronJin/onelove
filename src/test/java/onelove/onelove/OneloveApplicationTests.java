cc1df3016615589b4bc1034095d80c8f2817b9d0
IronJin <85132068+IronJin@users.noreply.github.com>
2022-10-24 05:06:39 +0900

    Initial commit

---
+++ src/test/java/onelove/onelove/OneloveApplicationTests.java
@@ -0,0 +1,13 @@
+package onelove.onelove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
